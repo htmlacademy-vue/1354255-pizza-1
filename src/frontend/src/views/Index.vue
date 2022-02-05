987c30de743ab10c3440eb84242521a373652d72
Maryana <ilon.mars@yandex.ru>
2022-02-05 18:47:32 +0300

    fix: autoclosing tags

--- src/frontend/src/views/Index.vue
+++ src/frontend/src/views/Index.vue
@@ -3,13 +3,10 @@
     <form action="#" method="post">
       <div class="content__wrapper">
         <h1 class="title title--big">Конструктор пиццы</h1>
-        <builder-dough-selector></builder-dough-selector>
-
-        <builder-size-selector></builder-size-selector>
-
-        <builder-ingredients-selector></builder-ingredients-selector>
-
-        <builder-pizza-view></builder-pizza-view>
+        <builder-dough-selector />
+        <builder-size-selector />
+        <builder-ingredients-selector />
+        <builder-pizza-view />
       </div>
     </form>
   </main>
