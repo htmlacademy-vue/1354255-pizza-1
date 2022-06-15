425b3f2886605518ea06b19e4b5064369972ceb4
Maryana <ilon.mars@yandex.ru>
2022-06-15 21:15:18 +0300

    add: get orders from server

--- src/frontend/src/store/modules/orders.store.js
+++ src/frontend/src/store/modules/orders.store.js
@@ -1,10 +1,38 @@
-const state = () => ({});
+const state = () => ({
+  orderList: [],
+  miscList: [],
+});
 
-const mutations = {};
+const mutations = {
+  SET_ORDERS: (state, orders) => {
+    state.orderList = orders;
+  },
 
-const actions = {};
+  SET_MISC: (state, misc) => {
+    state.miscList = misc;
+  },
+};
 
-const getters = {};
+const actions = {
+  async loadOrders({ commit }) {
+    const orders = await this.$api.orders.query();
+    commit("SET_ORDERS", orders);
+  },
+
+  async loadMisc({ commit }) {
+    const misc = await this.$api.misc.query();
+    commit("SET_MISC", misc);
+  },
+};
+
+const getters = {
+  getMisc:
+    (state) =>
+    ({ miscId, quantity }) => {
+      const currentMisc = state.miscList.find((item) => item.id === miscId);
+      return { ...currentMisc, quantity };
+    },
+};
 
 export default {
   namespaced: true,
--- src/frontend/src/views/Orders.vue
+++ src/frontend/src/views/Orders.vue
@@ -6,120 +6,109 @@
       </div>
 
       <section class="sheet order" v-if="orderList.length">
-        <div class="order__wrapper">
-          <div class="order__number">
-            <b>Заказ #11199929</b>
+        <div v-for="order in orderList" :key="order.id">
+          <div class="order__wrapper">
+            <div class="order__number">
+              <b>Заказ #{{ order.id }}</b>
+            </div>
+            <div class="order__sum">
+              <span>Сумма заказа: 1&nbsp;564 ₽</span>
+            </div>
+            <div class="order__button">
+              <button type="button" class="button button--border">
+                Удалить
+              </button>
+            </div>
+            <div class="order__button">
+              <button type="button" class="button">Повторить</button>
+            </div>
           </div>
 
-          <div class="order__sum">
-            <span>Сумма заказа: 1&nbsp;564 ₽</span>
-          </div>
+          <ul class="order__list">
+            <li
+              class="order__item"
+              v-for="pizza in order.orderPizzas"
+              :key="pizza.id"
+            >
+              <div class="product">
+                <img
+                  src="@/assets/img/product.svg"
+                  class="product__img"
+                  width="56"
+                  height="56"
+                  :alt="pizza.name"
+                />
+                <div class="product__text">
+                  <h2>{{ pizza.name }}</h2>
+                  <ul>
+                    <li>{{ pizza.sizeId }} см, на {{ pizza.doughId }} тесте</li>
+                    <li>Соус: {{ pizza.sauceId }}</li>
+                    <li>
+                      Начинка: грибы, лук, ветчина, пармезан, ананас, бекон, блю
+                      чиз
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <p class="order__price">782 ₽</p>
+            </li>
+          </ul>
 
-          <div class="order__button">
-            <button type="button" class="button button--border">Удалить</button>
-          </div>
-          <div class="order__button">
-            <button type="button" class="button">Повторить</button>
-          </div>
+          <ul class="order__additional" v-if="!!order.orderMisc">
+            <li
+              v-for="item in order.orderMisc"
+              :key="item.miscId"
+              class="order__additional-item"
+              :data-quantity="`x${getMisc(item).quantity}`"
+            >
+              <img
+                :src="getMisc(item).image"
+                width="20"
+                height="30"
+                :alt="getMisc(item).name"
+              />
+              <p>
+                <span>{{ getMisc(item).name }}</span>
+                <b>{{ getMisc(item).price }} ₽</b>
+              </p>
+            </li>
+          </ul>
+
+          <p class="order__address">
+            Адрес доставки: Тест (или если адрес новый - писать целиком)
+          </p>
         </div>
-
-        <ul class="order__list">
-          <li class="order__item">
-            <div class="product">
-              <img
-                src="img/product.svg"
-                class="product__img"
-                width="56"
-                height="56"
-                alt="Капричоза"
-              />
-              <div class="product__text">
-                <h2>Капричоза</h2>
-                <ul>
-                  <li>30 см, на тонком тесте</li>
-                  <li>Соус: томатный</li>
-                  <li>
-                    Начинка: грибы, лук, ветчина, пармезан, ананас, бекон, блю
-                    чиз
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <p class="order__price">782 ₽</p>
-          </li>
-          <li class="order__item">
-            <div class="product">
-              <img
-                src="img/product.svg"
-                class="product__img"
-                width="56"
-                height="56"
-                alt="Капричоза"
-              />
-              <div class="product__text">
-                <h2>Моя любимая</h2>
-                <ul>
-                  <li>30 см, на тонком тесте</li>
-                  <li>Соус: томатный</li>
-                  <li>Начинка: грибы, лук, ветчина, пармезан, ананас</li>
-                </ul>
-              </div>
-            </div>
-
-            <p class="order__price">2х782 ₽</p>
-          </li>
-        </ul>
-
-        <ul class="order__additional">
-          <li>
-            <img
-              src="img/cola.svg"
-              width="20"
-              height="30"
-              alt="Coca-Cola 0,5 литра"
-            />
-            <p>
-              <span>Coca-Cola 0,5 литра</span>
-              <b>56 ₽</b>
-            </p>
-          </li>
-          <li>
-            <img src="img/sauce.svg" width="20" height="30" alt="Острый соус" />
-            <span>Острый соус <br />30 ₽</span>
-          </li>
-          <li>
-            <img
-              src="img/potato.svg"
-              width="20"
-              height="30"
-              alt="Картошка из печи"
-            />
-            <p>
-              <span>Картошка из печи</span>
-              <b>170 ₽</b>
-            </p>
-          </li>
-        </ul>
-
-        <p class="order__address">
-          Адрес доставки: Тест (или если адрес новый - писать целиком)
-        </p>
       </section>
     </div>
   </main>
 </template>
 
 <script>
+import { mapState, mapGetters } from "vuex";
+
 export default {
-  data() {
-    return {
-      orderList: [],
-    };
+  computed: {
+    ...mapState("Orders", ["orderList"]),
+    ...mapGetters("Orders", ["getMisc"]),
   },
 
-  async created() {
-    this.orderList = await this.$api.orders.query();
+  created() {
+    this.$store.dispatch("Orders/loadOrders");
+    this.$store.dispatch("Orders/loadMisc");
   },
 };
 </script>
+
+<style lang="scss">
+.order__additional-item {
+  display: flex;
+  align-items: center;
+
+  &::after {
+    content: attr(data-quantity);
+    margin-left: 5px;
+    display: block;
+    color: #777;
+  }
+}
+</style>
